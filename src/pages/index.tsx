f678fb5a4fa1b9632cf3da5cb53db00f0e590349
Vanderlei Oliveira <vanderlei327@gmail.com>
2020-08-16 20:40:30 -0300

    Style header Homepage

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -6,6 +6,8 @@ import Search from '../components/search-form/search-form';
 import SearchWrapper from '../components/search-wrapper/search-wrapper';
 import Loading from '../components/loading/loading';
 
+import styles from './index.module.scss';
+
 const Home = (): JSX.Element => {
 
 	const [api, setApi] = useState(null);
@@ -30,13 +32,16 @@ const Home = (): JSX.Element => {
 	}
 
 	return (
-		<div>
+		<div className={api ? styles.show_search : styles.initial_layout}>
 			<Head>
 				<link rel="shortcut icon" href="favicon.ico"></link>
 				<title>O Cinéfilo - Buscador de séries e filmes</title>
 			</Head>
-			<header>
-				<h1>Movie Search</h1>
+			<header className={styles.site_header}>
+				<h1>
+					<span className="font-zero">O Cinéfilo</span>
+					<img className={styles.logo} src="/img/o_cinefilo.svg" alt="O Cinéfilo" />
+				</h1>
 				<Search onSubmit={handleSubmit} />
 			</header>
 
