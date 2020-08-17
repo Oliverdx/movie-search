2fb1a5164eb1151f078ea940b8efcfd4c78f8436
Vanderlei Oliveira <vanderlei327@gmail.com>
2020-08-16 21:26:24 -0300

    Search Wrapper Style

--- src/components/search-wrapper/search-wrapper.tsx
+++ src/components/search-wrapper/search-wrapper.tsx
@@ -1,20 +1,28 @@
 import React from 'react';
 
+import styles from './search-wrapper.module.scss';
+
 const SearchWrapper = ({ searchResults }): JSX.Element => {
 
   if (searchResults.message) {
     return <div><h2>{searchResults.message}</h2></div>
   }
 
-  return <div>
+  return <div className={styles.search_wrapper}>
     {searchResults && searchResults.map((movie, index) => {
       const moviePoster = movie?.Poster === 'N/A' ?
         'https://via.placeholder.com/300x367' :
         movie.Poster;
 
-      return <a href={`/${movie?.Type}/${movie?.Title}`} key={index}>
-        <h2>{movie?.Title}</h2>
-        <img src={moviePoster} alt={movie?.Title} />
+      return <a href={`/${movie?.Type}/${movie?.Title}`} key={index} className={styles.search_item}>
+        <img className={styles.item_image} src={moviePoster} alt={movie?.Title} />
+        <div className={styles.item_info}>
+          <button className={styles.item_info__like}></button>
+          <div className={styles.item_info__bottom}>
+            <h2 className={styles.item_info__title}>{movie?.Title}</h2>
+            <p className={styles.item_info__year}>{movie?.Year}</p>
+          </div>
+        </div>
       </a>
     })}
   </div>
